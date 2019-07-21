812dc72242abd809a757335f85cd0c3d931a4590
vcmsss <pak.al.ig@yandex.ru>
2019-07-21 19:28:10 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+    public static void main(String args[]) {
+
+        for (int i = 1; i < 51; i++) {
+
+            System.out.println(i);
+        }
+    }
+}
